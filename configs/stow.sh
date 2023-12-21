67c513a055ed57814f56c2fdc3b645dfbd76e772
Andreas Nigg <andreas.nigg@devopsandmore.com>
2023-12-21 19:51:37 +0100

    changes

--- configs/stow.sh
+++ configs/stow.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env zsh
 
 if [[ -z $STOW_FOLDERS ]]; then
-	STOW_FOLDERS="alacritty,karabiner,nvim,starship,tmux,zsh"
+	STOW_FOLDERS="alacritty,karabiner,nvim,starship,tmux"
 fi
 
 if [[ -z $DOTFILES ]]; then
-	DOTFILES=$HOME/.dotfiles
+	DOTFILES=$HOME/.dotfiles/configs
 fi
 
 pushd $DOTFILES
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g"); do
 	echo "stow $folder"
-	stow -D $folder
-	stow $folder
+	stow -D $folder -t $HOME
+	stow $folder -t $HOME
 done
+
+# if macos, stow zsh_macos, otherwise zsh_linux
+if [[ $(uname) == "Darwin" ]]; then
+    stow zsh_macos -t $HOME
+else
+    stow zsh_linux -t $HOME
+fi
+
 popd
