bc4bfbc96f70540d3ff2992bb30f2ae083fbe4ba
Andreas Nigg <andreas.nigg@devopsandmore.com>
2024-01-11 14:25:51 +0100

    fix new note extension

--- configs/scripts/scripts/new_note.sh
+++ configs/scripts/scripts/new_note.sh
@@ -31,4 +31,4 @@ fi
 touch "$HOME/.notes/reference/$IDEA_TYPE/$FILE_NAME"
 
 # Open the file in nvim
-cd $HOME/.notes/reference/$IDEA_TYPE && nvim "$HOME/.notes/reference/$IDEA_TYPE/$FILE_NAME.norg"
+cd $HOME/.notes/reference/$IDEA_TYPE && nvim "$HOME/.notes/reference/$IDEA_TYPE/$FILE_NAME.md"
