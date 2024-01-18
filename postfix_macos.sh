e9913e3c634eefe554f1e5e089170ba3567e3fc7
Andreas Nigg <andreas.nigg@devopsandmore.com>
2024-01-18 08:15:26 +0100

    add yabai skhd and sketchybar

---
+++ postfix_macos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Hiding Dock and menu bar"
+read -p "Is your dock currently hidden? (y/n)" dock_hide
+if [ $dock_hide == "n" ]; then
+	osascript -e "tell application \"System Events\" to set the autohide of the dock preferences to true"
+fi
+
+read -p "Is your menu bar currently hidden? (y/n)" menu_hide
+if [ $menu_hide == "n" ]; then
+	osascript -e 'tell application "System Events"
+    tell dock preferences to set autohide menu bar to not autohide menu bar
+    end tell'
+fi
+
+echo "make sure dark mode is activated"
+osascript -e 'tell application "System Events"
+    tell appearance preferences
+        set dark mode to true
+    end tell
+end tell'
+
+echo "Starting services"
+brew services restart sketchybar
+yabai --start-service
+skhd --start-service
+echo "That's It!"
