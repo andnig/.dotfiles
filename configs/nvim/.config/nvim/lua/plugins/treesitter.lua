b8e4b34dc702c745f7952deff983b909b04eaeeb
Andreas Nigg <andreas.nigg@devopsandmore.com>
2024-01-31 19:40:48 +0100

    treesitter dbt conf

--- configs/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,10 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         opts = {
+            highlight = {
+                enable = true,
+                disable = { "dbt", "sql" },
+            },
             ensure_installed = {
                 "bash",
                 "csv",
