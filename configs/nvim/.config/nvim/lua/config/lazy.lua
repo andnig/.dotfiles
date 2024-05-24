dd4e482129bc2922b0881e8e6ef2e71ae50a921e
Andreas Nigg <andreas.nigg@devopsandmore.com>
2024-05-24 16:16:47 +0200

    copilot fix?

--- configs/nvim/.config/nvim/lua/config/lazy.lua
+++ configs/nvim/.config/nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.linting.eslint" },
         { import = "lazyvim.plugins.extras.formatting.prettier" },
         { import = "lazyvim.plugins.extras.dap.core" },
-
+        { import = "lazyvim.plugins.extras.coding.copilot" },
         { import = "lazyvim.plugins.extras.editor.harpoon2" },
         { import = "lazyvim.plugins.extras.editor.illuminate" },
         { import = "lazyvim.plugins.extras.editor.mini-move" },
