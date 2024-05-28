3bed2d6c91c60487b57a4339cc83803aee836ec1
Andreas Nigg <andreas.nigg@devopsandmore.com>
2024-05-28 09:49:58 +0200

    fixes

--- configs/nvim/.config/nvim/lua/plugins/lsp.lua
+++ configs/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -8,11 +8,5 @@ return {
     { "nvim-neotest/nvim-nio" }, -- required for nvim dap
     {
         "neovim/nvim-lspconfig",
-        opts = {
-            ---@type lspconfig.options
-            servers = {
-                prismals = {},
-            },
-        },
     },
 }
