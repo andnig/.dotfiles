bc6152bd771d11c11932f379c423e0ff3d9bfd2e
Andreas Nigg <andreas.nigg@devopsandmore.com>
2024-06-10 19:58:31 +0200

    new version of virtual env selector

--- configs/nvim/.config/nvim/lua/plugins/venv-selector.lua
+++ configs/nvim/.config/nvim/lua/plugins/venv-selector.lua
@@ -1,24 +1,46 @@
 return {
+    --     {
+    --         "linux-cultist/venv-selector.nvim",
+    --         dependencies = {
+    --             "neovim/nvim-lspconfig",
+    --             "nvim-telescope/telescope.nvim",
+    --             "mfussenegger/nvim-dap-python",
+    --         },
+    --         opts = function(_, opts)
+    --             if require("lazyvim.util").has("nvim-dap-python") then
+    --                 opts.dap_enabled = true
+    --             end
+    --             return vim.tbl_deep_extend("force", opts, {
+    --                 name = {
+    --                     ".venv",
+    --                     ".env",
+    --                 },
+    --                 anaconda_base_path = os.getenv("CONDA_BASE_PREFIX"),
+    --                 anaconda_envs_path = os.getenv("CONDA_BASE_PREFIX") .. "/envs",
+    --             })
+    --         end,
+    --         keys = {
+    --             { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" },
+    --             { "<leader>cc", "<cmd>:VenvSelectCached<cr>", desc = "Select Cached VirtualEnv" },
+    --             { "<leader>cm", "<cmd>:VenvSelectCached<cr>", desc = "Select My VirtualEnv" },
+    --         },
+    --     },
     {
         "linux-cultist/venv-selector.nvim",
-        cmd = "VenvSelect",
-        opts = function(_, opts)
-            if require("lazyvim.util").has("nvim-dap-python") then
-                opts.dap_enabled = true
-            end
-            return vim.tbl_deep_extend("force", opts, {
-                name = {
-                    ".venv",
-                    ".env",
+        opts = {
+            settings = {
+                search = {
+                    anaconda_envs = {
+                        command = "fdfind bin/python$ $CONDA_BASE_PREFIX/envs --full-path --color never -E /proc", -- change path here to your anaconda envs
+                    },
+                    anaconda_base = {
+                        command = "fdfind /python$ $CONDA_BASE_PREFIX/bin --full-path --color never -E /proc", -- change path here to your anaconda base
+                    },
                 },
-                anaconda_base_path = os.getenv("CONDA_BASE_PREFIX"),
-                anaconda_envs_path = os.getenv("CONDA_BASE_PREFIX") .. "/envs",
-            })
-        end,
-        keys = {
-            { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" },
-            { "<leader>cc", "<cmd>:VenvSelectCached<cr>", desc = "Select Cached VirtualEnv" },
-            { "<leader>cm", "<cmd>:VenvSelectCached<cr>", desc = "Select My VirtualEnv" },
+                options = {
+                    notify_user_on_venv_activation = true,
+                },
+            },
         },
     },
 }
