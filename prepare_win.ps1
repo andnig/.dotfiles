575637f487fa01a1c6c8f95b53990cf503fa30a9
Andreas Nigg <andreas.nigg@devopsandmore.com>
2024-10-05 12:33:22 +0200

    add memreduct

--- prepare_win.ps1
+++ prepare_win.ps1
@@ -39,6 +39,7 @@ winget install -e --id glzr-io.glazewm --accept-package-agreements --accept-sour
 winget install -e --id Flameshot.Flameshot --accept-package-agreements --accept-source-agreements
 winget install -e --id=TheBrowserCompany.Arc --accept-package-agreements --accept-source-agreements
 winget install -e --id=AgileBits.1Password --accept-package-agreements --accept-source-agreements
+winget install -e --id=Henry++.MemReduct --accept-package-agreements --accept-source-agreements
 
 # Installing the WSL2 distro
 wsl --update
